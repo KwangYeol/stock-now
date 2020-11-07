3a1c98932b9efe023131efe395dd504059050b1d
Ryu Kwangyeol <kwangyeolryu@gmail.com>
2020-11-08 00:10:44 +0900

    r 4.0.3 f

--- hello.R
+++ hello.R
@@ -2,5 +2,9 @@ source("R/kuant.R")
 
 # ==> Step 1. get tickers
 tickers <- get_tickers()
+print(head(tickers))
+print(getwd())
+print(list.files(getwd()))
+
 write_tickers(tickers)
 # tickers <- read_parquet("obs/tickers.parquet")
